34de2b7f730b95eb51b456c94f158e94f464e56e
vijayswarnkar21 <vijay.swarnkar.vdn@gmail.com>
2026-07-17 21:33:03 +0530

    LLM based chat boat with python memory

--- chatbot.py
+++ chatbot.py
@@ -5,10 +5,16 @@ load_dotenv()
 
 model = ChatAnthropic(model='claude-haiku-4-5')
 
+# The concept of memory will be in phase 2
+memory = []
+
 while True:
     user_input = input('you: ')
+    memory.append(user_input)
     if user_input == 'stop':
         break
-    result=model.invoke(user_input)
+    # in phase 1 only user_input will be passed
+    result=model.invoke(memory)
+    memory.append(result.content)
     print('AI: ',result.content)
         
